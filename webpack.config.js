6c5f1ae5e98cae308e7dc1ddd1441fc7a6b4ae72
Kislax <maxpodstrechny@gmail.com>
2020-12-22 17:25:20 +0200

    add first commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin} = require('clean-webpack-plugin');
+
+module.exports = {
+
+    entry: {
+        main: path.resolve(__dirname, './src/index.js')
+    },
+    output:{
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'),
+            filename: 'index.html',
+        }),
+    ],
+    module: {
+        rules:[
+            //javascript
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use: ['babel-loader']
+            },
+        ],
+    },
+}
